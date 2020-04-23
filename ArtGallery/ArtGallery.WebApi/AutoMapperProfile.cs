9dfa50d341884e874d2b4d0929fa81d0ae01d43a
Anna <kichanna53@gmail.com>
2020-04-23 15:54:34 +0300

    Added webapi

---
+++ ArtGallery/ArtGallery.WebApi/AutoMapperProfile.cs
@@ -0,0 +1,27 @@
+﻿using ArtGallery.Client.DTO.Read;
+using ArtGallery.Client.Requests.Create;
+using ArtGallery.Client.Requests.Update;
+using ArtGallery.Domain.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtGallery.WebApi
+{
+    public class AutoMapperProfile: Profile
+    {
+        public AutoMapperProfile()
+        {
+            this.CreateMap<DataAccess.Entities.Sketch, Domain.Sketch>();
+            this.CreateMap<DataAccess.Entities.Category, Domain.Category>();
+            this.CreateMap<Domain.Sketch, SketchDTO>();
+            this.CreateMap<Domain.Category, CategoryDTO>();
+            this.CreateMap<SketchCreateDTO, SketchUpdateModel>();
+            this.CreateMap<SketchUpdateDTO, SketchUpdateModel>();
+            this.CreateMap<SketchUpdateModel, DataAccess.Entities.Sketch>();
+        }
+        
+    }
+}
--- ArtGallery/ArtGallery.WebApi/Startup.cs
+++ ArtGallery/ArtGallery.WebApi/Startup.cs
@@ -2,20 +2,51 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ArtGallery.BLL.Contracts;
+using ArtGallery.BLL.Implementation;
+using ArtGallery.DataAccess.Context;
+using ArtGallery.DataAccess.Contracts;
+using ArtGallery.DataAccess.Implementations;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace ArtGallery.WebApi
 {
     public class Startup
     {
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddAutoMapper(typeof(Startup));
+
+            // BLL
+            services.Add(new ServiceDescriptor(typeof(ISketchCreateService), typeof(SketchCreateService), ServiceLifetime.Scoped));
+            services.Add(new ServiceDescriptor(typeof(ISketchGetService), typeof(SketchGetService), ServiceLifetime.Scoped));
+            services.Add(new ServiceDescriptor(typeof(ISketchUpdateService), typeof(SketchUpdateService), ServiceLifetime.Scoped));
+            services.Add(new ServiceDescriptor(typeof(ICategoryGetService), typeof(CategoryGetService), ServiceLifetime.Scoped));
+
+            // DataAccess
+            services.Add(new ServiceDescriptor(typeof(ISketchDataAccess), typeof(SketchDataAccess), ServiceLifetime.Transient));
+            services.Add(new ServiceDescriptor(typeof(ICategoryDataAccess), typeof(CategoryDataAccess), ServiceLifetime.Transient));
+
+            // DB Contexts
+            services.AddDbContext<SketchDirectoryContext>(options =>
+                options.UseSqlServer(this.Configuration.GetConnectionString("SketchDirectory")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +57,13 @@ namespace ArtGallery.WebApi
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
